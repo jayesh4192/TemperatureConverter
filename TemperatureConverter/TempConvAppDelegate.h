f28309de511cb76cae4e0f1b27661e07d60b74a7
Jayesh Shah <jayeshs@numbertrain-lm.(none)>
2013-07-25 21:06:16 -0700

    Initial Commit

---
+++ TemperatureConverter/TempConvAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TempConvAppDelegate.h
+//  TemperatureConverter
+//
+//  Created by Jayesh Shah on 7/25/13.
+//  Copyright (c) 2013 Y.CORP.YAHOO.COM\jayeshs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TempConvAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TemperatureConverterTests/TemperatureConverterTests.h
@@ -0,0 +1,13 @@
+//
+//  TemperatureConverterTests.h
+//  TemperatureConverterTests
+//
+//  Created by Jayesh Shah on 7/25/13.
+//  Copyright (c) 2013 Y.CORP.YAHOO.COM\jayeshs. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TemperatureConverterTests : SenTestCase
+
+@end
